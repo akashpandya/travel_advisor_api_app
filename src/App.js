9f3b18408a3bd8f5d104e77fef29ef045a5b1aba
akashpandya <akash2pandya@gmail.com>
2024-02-27 16:05:25 -0500

    paystop

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+import { CssBaseline, Grid } from "@material-ui/core";
+
+const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+export default App;
